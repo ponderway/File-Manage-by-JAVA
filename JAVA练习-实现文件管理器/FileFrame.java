3259856308672537c0e86a46c5389bcd5e69fa2a
weixiaoxian <weixiaoxian1995@163.com>
2015-12-07 11:17:40 +0800

    add The right-click menu
    增加了鼠标右键菜单的实现，完成了打开，删除，新建文件夹的功能

--- JAVA练习-实现文件管理器/FileFrame.java
+++ JAVA练习-实现文件管理器/FileFrame.java
@@ -15,7 +15,9 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.event.ListSelectionEvent;
@@ -36,7 +38,8 @@ public class FileFrame extends JFrame{
 	
 	private FileListPanel filelistShow;
 	private PathPanel pathPanel;
-	String mouseSelectFileName;
+	private String mouseSelectFileName;
+	private MouseRightPopup popup;
 	
 	public FileFrame()
 	{
@@ -44,6 +47,7 @@ public class FileFrame extends JFrame{
 		filelistShow=new FileListPanel();
 		pathPanel=new PathPanel();
 		mouseSelectFileName=new String("");
+		popup=new MouseRightPopup();
 		
 		//设置窗口关闭方法
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,8 +61,11 @@ public class FileFrame extends JFrame{
 		//实现路径文本框可以直接控制目录跳转
 		pathTextToFile();
 		
-		//实现鼠标控制文件显示列表
+		//实现鼠标控制文件显示列表和右键显示菜单
 		mouseControlFilelist();
+		
+		//实现右键菜单功能
+		mouseRightMenuFunction();
 		
 		//控制组件摆放位置
 		this.add(filelistShow,BorderLayout.CENTER);
@@ -69,6 +76,21 @@ public class FileFrame extends JFrame{
 	{
 		pathPanel.showpath(path);
 		return filelistShow.openFile(path);
+	}
+	
+	public boolean deleteFile(String path)
+	{
+		boolean temp= filelistShow.deleteFile(path);
+		File f=new File(path);
+		openFile(f.getParent());
+		return temp;
+	}
+	
+	public boolean createDir(String parentPath)
+	{
+		boolean temp=filelistShow.createDir(parentPath);
+		openFile(parentPath);
+		return temp;
 	}
 	
 	private void pathTextToFile()
@@ -109,13 +131,20 @@ public class FileFrame extends JFrame{
 			@Override 
 			public void mouseClicked(MouseEvent e)
 			{
+				//设置左键双击打开文件夹
 				if(filelistShow.getItemCanClick())
 				{
-					if(e.getClickCount()==2)
+					if(e.getClickCount()==2 && e.getButton()==MouseEvent.BUTTON1)
 					{
 						openFile(pathPanel.getPathInput()+"\\"+mouseSelectFileName);
 						mouseSelectFileName="";
 					 }
+				}
+				
+				//设置右键显示菜单
+				if(e.getButton()==MouseEvent.BUTTON3)
+			    {
+					popup.show(e.getComponent(), e.getX(), e.getY());
 				}
 			}
 		});
@@ -144,16 +173,46 @@ public class FileFrame extends JFrame{
 		}
 		return temp.toString();
 	}
+	
+	public void mouseRightMenuFunction()
+	{
+		ActionListener itemAction=new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根
+				String temp = e.getActionCommand();
+				if(temp.equals("打开"))
+				{
+					openFile(pathPanel.getPathInput()+"\\"+mouseSelectFileName);
+				}
+				if(temp.equals("删除"))
+				{
+					deleteFile(pathPanel.getPathInput()+"\\"+mouseSelectFileName);
+				}
+				if(temp.equals("新建文件夹"))
+				{
+					createDir(pathPanel.getPathInput());
+				}
+				
+			}
+		};
+		
+		
+		popup.addItemListener(0,itemAction);
+		popup.addItemListener(1, itemAction);
+		popup.addItemListener(2, itemAction);
+	}
 }
 
 //文件列表面板
 class FileListPanel extends JPanel 
 {
-	DefaultListModel<String> item;
-	JList<?> filelist;
-	JScrollPane basepanel;
-	String localPath;
-	String[] manyFiles={""};
+	private DefaultListModel<String> item;
+	private JList<?> filelist;
+	private JScrollPane basepanel;
+	private String localPath;
+	private String[] manyFiles={""};
 	
 	
 	public FileListPanel()
@@ -225,6 +284,51 @@ class FileListPanel extends JPanel
 		return item.size()!=0 && 
 				!Objects.equals(item.firstElement(),"This is not a Directory"); 
 	}
+	
+	public boolean deleteFile(String path)  //删除文件的公有接口
+	{
+		File f=new File(path);
+		if(f.exists() )
+		{  
+			if(!f.isDirectory())
+				return f.delete();
+			else
+				return deleteDirectory(f);
+		}
+		return false;
+	}
+	
+	private boolean deleteDirectory(File f)   //递归删除目录下所有文件
+	{
+		String [] children = f.list();
+		if(children==null)   return f.delete();  //没有子目录则直接删除
+		for(String ch : children)
+		{
+			File file=new File(f, ch);
+			if(file.isDirectory())
+				deleteDirectory(file);
+			else
+				file.delete();
+		}
+		f.delete();  //目录为空时可以直接删除
+		return true;
+	}
+	
+	public boolean createDir(String parentPath)  //在parentPath目录下创建文件夹
+	{
+		File f=new File(parentPath);
+		if(!f.exists() || !f.isDirectory())  return false;
+		String [] child=f.list();
+		int i =0;    //父目录下有多少“新建文件夹”前缀的文件
+		if(child!=null)
+		{
+			for(String ch : child)
+				if(ch.startsWith("新建文件夹")) i++;
+		}
+		File newDir=new File(f, "新建文件夹"+"("+i+")");
+		newDir.mkdir();
+		return true;
+	}
 }
 
 //路径显示面板和按钮
@@ -271,6 +375,34 @@ class PathPanel extends JPanel
 	
 }
 
+class MouseRightPopup extends JPopupMenu
+{
+	private JMenuItem[] item;
+	
+	
+	public MouseRightPopup()
+	{
+		super();
+		item=new JMenuItem[5];
+		
+		item[0]=new JMenuItem("打开");
+		item[1]=new JMenuItem("删除");
+		item[2]=new JMenuItem("新建文件夹");
+		item[3]=new JMenuItem("新建文件");
+		item[4]=new JMenuItem("刷新");
+		
+		this.add(item[0]);
+		this.add(item[1]);
+		this.add(item[2]);
+		this.add(item[3]);
+		this.add(item[4]);
+	}
+	
+	public void addItemListener(int i,ActionListener a)
+	{
+		item[i].addActionListener(a);
+	}
+}
 
 
 
